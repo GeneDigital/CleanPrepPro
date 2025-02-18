a11c0f34d0ca7336b44bdf614469211d913327a8
Anthony Wright <info@genedigital.co.uk>
2025-02-18 15:37:32 +0000

    Initial Commit

---
+++ CleanPrepPro/CleanPrepProApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanPrepProApp.swift
+//  CleanPrepPro
+//
+//  Created by admin on 18/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct CleanPrepProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
